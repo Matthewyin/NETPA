1d3fcb8ac66f2360c0645b423f8dc2b22209bf19
MatthewYin <2738550@qq.com>
2018-11-22 00:56:35 +0800

    测试环境

--- TestEnvironment/VxlanEnv/DeviceConnect.py
+++ TestEnvironment/VxlanEnv/DeviceConnect.py
@@ -3,7 +3,7 @@
 
 from ncclient import manager
 import difflib
-import sys
+
 #带外管理交换机
 mgntsw_dict = { "host" : "172.20.13.221",
            "port" : "56332",
@@ -13,7 +13,7 @@ mgntsw_dict = { "host" : "172.20.13.221",
            "device_params" : {'name' : 'h3c'}}
 
 mgntSw = manager.connect(**mgntsw_dict)
-mgntSw_startupconfig = open('../Documention/mgntSw_startupconfig.xml','w')
+mgntSw_startupconfig = open('/Users/matthewyin/GitHub/NETPA/Documention/mgntSw_startupconfig.xml','w')
 mgntSw_config = mgntSw.get_config(source="running")
 print(mgntSw_config, file=mgntSw_startupconfig)
 
@@ -26,7 +26,7 @@ borderleaf_dict = { "host" : "172.20.13.222",
            "device_params" : {'name' : 'h3c'}}
 borderLeaf = manager.connect(**borderleaf_dict)
 borderLeaf_config = borderLeaf.get_config(source="running")
-borderLeaf_startupconfig = open('../Documention/borderLeaf_startupconfig.xml','w')
+borderLeaf_startupconfig = open('/Users/matthewyin/GitHub/NETPA/Documention/borderLeaf_startupconfig.xml','w')
 print(borderLeaf_config, file=borderLeaf_startupconfig)
 
 
@@ -39,7 +39,7 @@ Spine_dict = { "host" : "172.20.13.223",
            "device_params" : {'name' : 'h3c'}}
 Spine = manager.connect(**Spine_dict)
 Spine_config = Spine.get_config(source="running")
-Spine_startupconfig = open('../Documention/Spine_startupconfig.xml','w')
+Spine_startupconfig = open('/Users/matthewyin/GitHub/NETPA/Documention/Spine_startupconfig.xml','w')
 print(Spine_config, file=Spine_startupconfig)
 
 #AccessLeaf交换机
@@ -51,7 +51,7 @@ accessleaf01_dict = { "host" : "172.20.13.224",
            "device_params" : {'name' : 'h3c'}}
 accessLeaf01 = manager.connect(**accessleaf01_dict)
 accessLeaf01_config = accessLeaf01.get_config(source="running")
-accessLeaf01_startupconfig = open('../Documention/accessLeaf01_startupconfig.xml','w')
+accessLeaf01_startupconfig = open('/Users/matthewyin/GitHub/NETPA/Documention/accessLeaf01_startupconfig.xml','w')
 print(accessLeaf01_config, file=accessLeaf01_startupconfig)
 
 accessleaf02_dict = { "host" : "172.20.13.225",
@@ -62,7 +62,7 @@ accessleaf02_dict = { "host" : "172.20.13.225",
            "device_params" : {'name' : 'h3c'}}
 accessLeaf02 = manager.connect(**accessleaf02_dict)
 accessLeaf02_config = accessLeaf01.get_config(source="running")
-accessLeaf02_startupconfig = open('../Documention/accessLeaf02_startupconfig.xml','w')
+accessLeaf02_startupconfig = open('/Users/matthewyin/GitHub/NETPA/Documention/accessLeaf02_startupconfig.xml','w')
 print(accessLeaf02_config, file=accessLeaf02_startupconfig)
 
 #
@@ -80,9 +80,9 @@ def readFile(filename):
     fileHandle.close()
     return text
 
-text1_line = readFile('../Documention/accessLeaf01_startupconfig.xml')
-text2_line = readFile('../Documention/accessLeaf01_startupconfig.xml')
+text1_line = readFile('/Users/matthewyin/GitHub/NETPA/Documention/accessLeaf01_startupconfig.xml')
+text2_line = readFile('/Users/matthewyin/GitHub/NETPA/Documention/accessLeaf01_startupconfig.xml')
 
 d = difflib.HtmlDiff()
-compareFile = open('../Documention/comparefile.html','w')
+compareFile = open('/Users/matthewyin/GitHub/NETPA/Documention/comparefile.html','w')
 print(d.make_file(text1_line, text2_line), file=(compareFile))
---
+++ TestEnvironment/VxlanEnv/__init__.py
@@ -0,0 +1,2 @@
+#!/usr/bin/evn python
+# -*- conding:utf-8 -*-
